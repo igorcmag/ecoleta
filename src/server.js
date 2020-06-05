ca1cd235784aaaa082e22ce0f91ba0a91effdcf2
Igor Caldeira <igorcmag@gmail.com>
2020-06-04 22:20:41 -0300

    day four part one

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar caminhos
+//pagina inicial
+server.get("/", (req, res) => {
+    return res.render("index.html")   
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")   
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")   
+})
+
+//ligar o servidor
+server.listen(3000)
